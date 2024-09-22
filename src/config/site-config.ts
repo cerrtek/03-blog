98f51c5382f9faf90ac3e68a37cbfa350447b15c
Jose Luis GitHub <cerrtek@gmail.com>
2024-09-22 19:26:54 +0200

    Initial commit

---
+++ src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    title: 'Jose Luis Marques',
+    description: 'Blog Oficial de Jose Luis Marques - Web Developper'
+}
---
+++ src/content/config.ts
@@ -0,0 +1,42 @@
+import { defineCollection, reference, z } from "astro:content";
+
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        date: z.date(),
+        description: z.string(),
+        image: image().refine((img) => img.width < 1200, {
+            message: 'La imagen debe tener un tamaño inferior a 1200 px.'
+        } ),
+
+        // Relación
+        // author: z.string(),
+        author: reference('author'),
+
+        // Relación
+        tags: z.array(z.string()),
+
+        // Mostrar entrada
+        isDraft: z.boolean().default(false),
+    }),
+});
+
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({ image }) =>
+      z.object({
+        name: z.string(),
+        avatar: image(),
+        twitter: z.string(),
+        linkedIn: z.string(),
+        github: z.string(),
+        bio: z.string(),
+        subtitle: z.string(),
+      }),
+  });
+  
+export const collections = {
+    blog: blogCollection,
+    author: authorCollection
+};
