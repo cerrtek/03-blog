1f0b050c438631f6d86480df5a5e64858824f66a
Jose Luis GitHub <cerrtek@gmail.com>
2024-09-22 19:34:04 +0200

    First Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: 'https://example2.com'
+  site: 'https://example.com'
 });
